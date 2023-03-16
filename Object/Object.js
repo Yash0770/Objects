71df6cc3dc4c4bcebd1c896df144a54f942013d1
Yash <100015221+Yash0770@users.noreply.github.com>
2023-03-16 17:35:56 +0530

    Add files via upload

---
+++ Object/Object.js
@@ -0,0 +1,21 @@
+const obj = {                                   //creating a OBJECT
+    "subject1" : "Math",
+    subject2 : "Physics",
+    subject3 : "Chemistry",
+    value1 : 2,
+    yesOrNo : true,
+    value2 : undefined
+}
+
+console.log(obj["subject1"]);                   //calling Object
+console.log(obj.subject2);
+
+console.log("Below is called");
+console.log(obj.subject1);
+console.log(obj.subject2);
+console.log(obj.subject3);
+console.log(obj.value1);
+console.log(obj.yesOrNo);
+console.log(obj.value2);
+
+// console.log(obj.subject1);
